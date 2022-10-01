b679163273cfaf1ae2691a653030e4086c345087
Marco Aurélio Lima Fernandes <malf88@gmail.com>
2022-10-01 00:02:17 -0300

    Correção do horizon

--- app/Application/Http/Kernel.php
+++ app/Application/Http/Kernel.php
@@ -30,6 +30,14 @@ class Kernel extends HttpKernel
      * @var array<string, array<int, class-string|string>>
      */
     protected $middlewareGroups = [
+        'web' => [
+            \App\Application\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Application\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
         'api' => [
             \App\Application\Http\Middleware\JsonResponseMiddleware::class,
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
